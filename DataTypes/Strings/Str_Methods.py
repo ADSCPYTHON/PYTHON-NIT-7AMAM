7360d63c880e0287f6a17ac230c47e99240fad57
ADSCPYTHON <a.vision2050@hotmail.com>
2018-10-14 12:49:28 -0400

    todaycommit

---
+++ DataTypes/Strings/Str_Methods.py
@@ -0,0 +1,16 @@
+
+
+
+#!/usr/bin/python
+
+str1 = "zyxbcdef09"
+print ("Min character: " + min(str1))
+print ("Max character: " + max(str1))
+
+str2 = "ABCDEFZabcde"
+print ("Min character: " + min(str2))
+print ("Max character: " + max(str2))
+
+#0 - 9 =
+#A-Z   =
+#a-z   =
